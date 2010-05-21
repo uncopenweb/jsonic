d434cda6aabfe4e84872a940c6dee867037fda62
Peter Parente <parente@cs.unc.edu>
2010-05-20 21:25:50 -0400

    Continued unit test work for issue #18
    * Concurrent say / play tests
    * More simple tests with property changes

--- tests/concurrent.js
+++ tests/concurrent.js
@@ -1,9 +1,146 @@
 dojo.provide('info.mindtrove.tests.concurrent');
 
 module('concurrent', MODOPTS);
-// say 1 while say 1
-// say 1 while say 2
-// say while play
-// play while say
-// play 1 while play 1
-// play 1 while play 2
+test('say 1 while say 1', 4, function() {
+    stop(TO);
+    var ch1 = [0,0];
+    var ch2 = [0,0];
+    var def1 = this.js.say({text : UT1});
+    var def2 = this.js.say({text : UT1, channel : 'other'});
+    def1.callBefore(function() {
+        ok(!ch1[0] && !ch1[1], 'utterance started on default channel');
+        ch1[0]++;
+    }).callAfter(function(completed) {
+        ok(ch1[0] && !ch1[1] && completed, 'utterance finished on default channel');
+        ch1[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+    def2.callBefore(function() {
+        ok(!ch2[0] && !ch2[1], 'utterance started on other channel');
+        ch2[0]++;
+    }).callAfter(function(completed) {
+        ok(ch2[0] && !ch2[1] && completed, 'utterance finished on other channel');
+        ch2[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+});
+
+test('say 1 while say 2', 4, function() {
+    stop(TO);
+    var ch1 = [0,0];
+    var ch2 = [0,0];
+    var def1 = this.js.say({text : UT1});
+    var def2 = this.js.say({text : UT2, channel : 'other'});
+    def1.callBefore(function() {
+        ok(!ch1[0] && !ch1[1], 'utterance 1 started on default channel');
+        ch1[0]++;
+    }).callAfter(function(completed) {
+        ok(ch1[0] && !ch1[1] && completed, 'utterance 1 finished on default channel');
+        ch1[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+    def2.callBefore(function() {
+        ok(!ch2[0] && !ch2[1], 'utterance 2 started on other channel');
+        ch2[0]++;
+    }).callAfter(function(completed) {
+        ok(ch2[0] && !ch2[1] && completed, 'utterance 2 finished on other channel');
+        ch2[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+});
+
+test('say while play', 4, function() {
+    stop(TO);
+    var ch1 = [0,0];
+    var ch2 = [0,0];
+    var def1 = this.js.say({text : UT1});
+    var def2 = this.js.play({url : SND1, channel : 'other'});
+    def1.callBefore(function() {
+        ok(!ch1[0] && !ch1[1], 'utterance started on default channel');
+        ch1[0]++;
+    }).callAfter(function(completed) {
+        ok(ch1[0] && !ch1[1] && completed, 'utterance finished on default channel');
+        ch1[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+    def2.callBefore(function() {
+        ok(!ch2[0] && !ch2[1], 'sound started on other channel');
+        ch2[0]++;
+    }).callAfter(function(completed) {
+        ok(ch2[0] && !ch2[1] && completed, 'sound finished on other channel');
+        ch2[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+});
+
+test('play while say', 4, function() {
+    stop(TO);
+    var ch1 = [0,0];
+    var ch2 = [0,0];
+    var def1 = this.js.play({url : SND1});
+    var def2 = this.js.say({text : UT1, channel : 'other'});
+    def1.callBefore(function() {
+        ok(!ch1[0] && !ch1[1], 'sound started on default channel');
+        ch1[0]++;
+    }).callAfter(function(completed) {
+        ok(ch1[0] && !ch1[1] && completed, 'sound finished on default channel');
+        ch1[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+    def2.callBefore(function() {
+        ok(!ch2[0] && !ch2[1], 'utterance started on other channel');
+        ch2[0]++;
+    }).callAfter(function(completed) {
+        ok(ch2[0] && !ch2[1] && completed, 'utterance finished on other channel');
+        ch2[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+});
+
+test('play 1 while play 1', 4, function() {
+    stop(TO);
+    var ch1 = [0,0];
+    var ch2 = [0,0];
+    var def1 = this.js.play({url : SND1});
+    var def2 = this.js.play({url : SND1, channel : 'other'});
+    def1.callBefore(function() {
+        ok(!ch1[0] && !ch1[1], 'sound started on default channel');
+        ch1[0]++;
+    }).callAfter(function(completed) {
+        ok(ch1[0] && !ch1[1] && completed, 'sound finished on default channel');
+        ch1[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+    def2.callBefore(function() {
+        ok(!ch2[0] && !ch2[1], 'sound started on other channel');
+        ch2[0]++;
+    }).callAfter(function(completed) {
+        ok(ch2[0] && !ch2[1] && completed, 'sound finished on other channel');
+        ch2[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+});
+
+test('play 1 while play 2', 4, function() {
+    stop(TO);
+    var ch1 = [0,0];
+    var ch2 = [0,0];
+    var def1 = this.js.play({url : SND1});
+    var def2 = this.js.play({url : SND2, channel : 'other'});
+    def1.callBefore(function() {
+        ok(!ch1[0] && !ch1[1], 'sound 1 started on default channel');
+        ch1[0]++;
+    }).callAfter(function(completed) {
+        ok(ch1[0] && !ch1[1] && completed, 'sound 1 finished on default channel');
+        ch1[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+    def2.callBefore(function() {
+        ok(!ch2[0] && !ch2[1], 'sound 2 started on other channel');
+        ch2[0]++;
+    }).callAfter(function(completed) {
+        ok(ch2[0] && !ch2[1] && completed, 'sound 2 finished on other channel');
+        ch2[1]++
+        if(ch1[1] && ch2[1]) {start();}
+    });
+});
--- tests/simple.js
+++ tests/simple.js
@@ -23,8 +23,36 @@ test('stop say', function () {
         start();
     });
 });
-test('loop say', 1, function() { });
-test('say volume', 1, function() { });
+test('loop say', 4, function() {
+    stop(TO*2);
+    var js = this.js;
+    var def1 = this.js.setProperty({name: 'loop', value: true});
+    def1.callBefore(function(value) {
+        ok(!value, 'looping off before');
+    }).callAfter(function(value) {
+        ok(value, 'looping on after');
+    });
+    var def2 = this.js.say({text : UT1});
+    def2.callBefore(function() {
+        ok(true, 'before say deferred invoked');
+        setTimeout(dojo.hitch(js, js.stop), 3000);
+    }).callAfter(function(completed) {
+        ok(!completed, 'after say deferred invoked on interrupt');
+        start();
+    });
+});
+test('say volume', 2, function() {
+    stop(TO*2);
+    var js = this.js;
+    var def1 = this.js.setProperty({name: 'volume', value: 0.1});
+    def1.callBefore(function(value) {
+        ok(value == 1.0, 'volume 1.0 before');
+    }).callAfter(function(value) {
+        ok(value == 0.1, 'volume 0.1 after');
+    });
+    var def2 = this.js.say({text : UT2});
+    def2.callAfter(function() { start(); });
+});
 test('say rate', 1, function() { });
 test('say voice', 1, function() { });
 test('play', function () {
@@ -37,8 +65,36 @@ test('play', function () {
         start();
     });
 });
-test('loop play', 1, function() { });
-test('play volume', 1, function() { });
+test('loop play', 4, function() { 
+    stop(TO*2);
+    var js = this.js;
+    var def1 = this.js.setProperty({name: 'loop', value: true});
+    def1.callBefore(function(value) {
+        ok(!value, 'looping off before');
+    }).callAfter(function(value) {
+        ok(value, 'looping on after');
+    });
+    var def2 = this.js.play({url : SND1});
+    def2.callBefore(function() {
+        ok(true, 'before play deferred invoked');
+        setTimeout(dojo.hitch(js, js.stop), 3000);
+    }).callAfter(function(completed) {
+        ok(!completed, 'after play deferred invoked on interrupt');
+        start();
+    });
+});
+test('play volume', 2, function() { 
+    stop(TO*2);
+    var js = this.js;
+    var def1 = this.js.setProperty({name: 'volume', value: 0.1});
+    def1.callBefore(function(value) {
+        ok(value == 1.0, 'volume 1.0 before');
+    }).callAfter(function(value) {
+        ok(value == 0.1, 'volume 0.1 after');
+    });
+    var def2 = this.js.play({url : SND2});
+    def2.callAfter(function() { start(); });
+});
 test('play error', function() {
     stop(TO);
     var def = this.js.play({url : SND1+'foobar'});
