9adc1a61cbd27dfa6e7ae58b8923cf6c1376a365
Peter Parente <parente@cs.unc.edu>
2011-06-26 13:33:10 -0400

    Sequential tests for wait method

--- tests/sequential.js
+++ tests/sequential.js
@@ -47,6 +47,34 @@ dojo.provide('uow.audio.tests.sequential');
                 start();
             });
         });
+        test('say 1, wait, say 2', 6, function() {
+            stop(TO*2);
+            var before = 0;
+            var after = 0;
+            var def1 = this.js.say({text : UT1});
+            var defW = this.js.wait({duration : 1000});
+            var def2 = this.js.say({text : UT2});
+            def1.callBefore(function() {
+                ok(before === 0, 'first utterance started first');
+                before = 1;
+            }).callAfter(function(completed) {
+                ok(after === 0 && completed, 'first utterance finished first');
+                after = 1;
+            });
+            defW.callBefore(function() {
+                ok(before === 1, 'wait started second');
+                before = 2;
+            }).callAfter(function(completed) {
+                ok(after === 1 && completed, 'wait finished second');
+                after = 2;
+            });
+            def2.callBefore(function() {
+                ok(before === 2, 'second utterance started third');
+            }).callAfter(function(completed) {
+                ok(after === 2 && completed, 'second utterance finished third');
+                start();
+            });
+        });
         test('say then play', 4, function() {
             stop(TO*2);
             var before = 0;
@@ -67,6 +95,34 @@ dojo.provide('uow.audio.tests.sequential');
                 start();
             });
         });
+        test('say, wait, play', 6, function() {
+            stop(TO*2);
+            var before = 0;
+            var after = 0;
+            var def1 = this.js.say({text : UT1});
+            var defW = this.js.wait({duration : 1000});
+            var def2 = this.js.play({url : SND1});
+            def1.callBefore(function() {
+                ok(before === 0, 'utterance started first');
+                before = 1;
+            }).callAfter(function(completed) {
+                ok(after === 0 && completed, 'utterance finished first');
+                after = 1;
+            });
+            defW.callBefore(function() {
+                ok(before === 1, 'wait started second');
+                before = 2;
+            }).callAfter(function(completed) {
+                ok(after === 1 && completed, 'wait finished second');
+                after = 2;
+            });
+            def2.callBefore(function() {
+                ok(before === 2, 'sound started third');
+            }).callAfter(function(completed) {
+                ok(after === 2 && completed, 'sound finished third');
+                start();
+            });
+        });
         test('play then say', 4, function() {
             stop(TO*2);
             var before = 0;
@@ -84,6 +140,34 @@ dojo.provide('uow.audio.tests.sequential');
                 ok(before === 1, 'utterance started second');
             }).callAfter(function(completed) {
                 ok(after === 1 && completed, 'utterance finished second');
+                start();
+            });
+        });
+        test('play, wait, say', 6, function() {
+            stop(TO*2);
+            var before = 0;
+            var after = 0;
+            var def1 = this.js.play({url : SND1});
+            var defW = this.js.wait({duration : 1000});
+            var def2 = this.js.say({text : UT1});
+            def1.callBefore(function() {
+                ok(before === 0, 'sound started first');
+                before = 1;
+            }).callAfter(function(completed) {
+                ok(after === 0 && completed, 'sound finished first');
+                after = 1;
+            });
+            defW.callBefore(function() {
+                ok(before === 1, 'wait started second');
+                before = 2;
+            }).callAfter(function(completed) {
+                ok(after === 1 && completed, 'wait finished second');
+                after = 2;
+            });
+            def2.callBefore(function() {
+                ok(before === 2, 'utterance started third');
+            }).callAfter(function(completed) {
+                ok(after === 2 && completed, 'utterance finished third');
                 start();
             });
         });
@@ -124,6 +208,34 @@ dojo.provide('uow.audio.tests.sequential');
                 ok(before === 1, 'second sound started second');
             }).callAfter(function(completed) {
                 ok(after === 1 && completed, 'second sound finished second');
+                start();
+            });
+        });   
+        test('play 1, wait, play 2', 6, function() {
+            stop(TO*2);
+            var before = 0;
+            var after = 0;
+            var def1 = this.js.play({url : SND1});
+            var defW = this.js.wait({duration: 1000});
+            var def2 = this.js.play({url : SND2});
+            def1.callBefore(function() {
+                ok(before === 0, 'first sound started first');
+                before = 1;
+            }).callAfter(function(completed) {
+                ok(after === 0 && completed, 'first sound finished first');
+                after = 1;
+            });
+            defW.callBefore(function() {
+                ok(before === 1, 'wait started second');
+                before = 2;
+            }).callAfter(function(completed) {
+                ok(after === 1 && completed, 'wait finished second');
+                after = 2;
+            });
+            def2.callBefore(function() {
+                ok(before === 2, 'second sound started third');
+            }).callAfter(function(completed) {
+                ok(after === 2 && completed, 'second sound finished third');
                 start();
             });
         });   
