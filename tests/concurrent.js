5df405eef58f53058a959d1607a02147afc92e0a
Peter Parente <parente@cs.unc.edu>
2011-06-26 14:11:46 -0400

    Concurrent tests for wait method

--- tests/concurrent.js
+++ tests/concurrent.js
@@ -79,6 +79,31 @@ dojo.provide('uow.audio.tests.concurrent');
             });
         });
 
+        test('say while wait', 4, function() {
+            stop(TO);
+            var ch1 = [0,0];
+            var ch2 = [0,0];
+            var def1 = this.js.say({text : UT1});
+            var def2 = this.js.wait({duration : 2000, channel : 'other'});
+            def1.callBefore(function() {
+                ok(!ch1[0] && !ch1[1], 'utterance started on default channel');
+                ch1[0]++;
+            }).callAfter(function(completed) {
+                ok(ch1[0] && !ch1[1] && completed, 'utterance finished on default channel');
+                ch1[1]++
+                if(ch1[1] && ch2[1]) {start();}
+            });
+            def2.callBefore(function() {
+                ok(!ch2[0] && !ch2[1], 'wait started on other channel');
+                ch2[0]++;
+            }).callAfter(function(completed) {
+                ok(ch2[0] && !ch2[1] && completed, 'wait finished on other channel');
+                ch2[1]++
+                if(ch1[1] && ch2[1]) {start();}
+            });
+        });
+
+
         test('play while say', 4, function() {
             stop(TO);
             var ch1 = [0,0];
@@ -102,6 +127,31 @@ dojo.provide('uow.audio.tests.concurrent');
                 if(ch1[1] && ch2[1]) {start();}
             });
         });
+        
+        test('play while wait', 4, function() {
+            stop(TO);
+            var ch1 = [0,0];
+            var ch2 = [0,0];
+            var def1 = this.js.play({url : SND1});
+            var def2 = this.js.wait({duration: 2000, channel : 'other'});
+            def1.callBefore(function() {
+                ok(!ch1[0] && !ch1[1], 'sound started on default channel');
+                ch1[0]++;
+            }).callAfter(function(completed) {
+                ok(ch1[0] && !ch1[1] && completed, 'sound finished on default channel');
+                ch1[1]++
+                if(ch1[1] && ch2[1]) {start();}
+            });
+            def2.callBefore(function() {
+                ok(!ch2[0] && !ch2[1], 'wait started on other channel');
+                ch2[0]++;
+            }).callAfter(function(completed) {
+                ok(ch2[0] && !ch2[1] && completed, 'wait finished on other channel');
+                ch2[1]++
+                if(ch1[1] && ch2[1]) {start();}
+            });
+        });
+
 
         test('play 1 while play 1', 4, function() {
             stop(TO);
