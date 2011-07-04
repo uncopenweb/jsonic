68e927e8c76de094ac02fce8067fc575248a23cf
Peter Parente <parente@cs.unc.edu>
2011-07-03 21:54:08 -0400

    Issue #27, test for pauseAll / unpauseAll across channels

--- tests/concurrent.js
+++ tests/concurrent.js
@@ -385,5 +385,36 @@ dojo.provide('uow.audio.tests.concurrent');
                 js.stopAll();
             }, 500);
         });
+        
+        test('say, pause all, unpause all', 4, function() {
+            stop(TO);
+            var js = this.js;
+            var ch1 = [0,0];
+            var ch2 = [0,0];
+            var def1 = this.js.say({text : UT1});
+            var def2 = this.js.say({text : UT2, channel : 'other'});
+            def1.callBefore(function() {
+                ok(!ch1[0] && !ch1[1], 'speech 1 started on default channel');
+                ch1[0]++;
+            }).callAfter(function(completed) {
+                ok(ch1[0] && !ch1[1] && completed, 'speech 1 interrupted on default channel');
+                ch1[1]++
+                if(ch1[1] && ch2[1]) {start();}
+            });
+            def2.callBefore(function() {
+                ok(!ch2[0] && !ch2[1], 'speech 2 started on other channel');
+                ch2[0]++;
+            }).callAfter(function(completed) {
+                ok(ch2[0] && !ch2[1] && completed, 'speech 2 interrupted on other channel');
+                ch2[1]++
+                if(ch1[1] && ch2[1]) {start();}
+            });
+            setTimeout(function() {
+                js.pauseAll();
+                setTimeout(function() {
+                    js.unpauseAll();
+                }, 2000);
+            }, 500);
+        });
     });
 })();
